5f16be9798d51ef0631959a568a10ea5c6a5283b
AbdelrahmanMahmoud74 <133714220+AbdelrahmanMahmoud74@users.noreply.github.com>
2023-11-12 15:13:16 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.querySelector(".fa-bars").addEventListener("click", () => {
+  document.querySelector(".nav-items").classList.toggle("active");
+});
+document.querySelectorAll(".nav-link").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelector(".nav-items").classList.toggle("active");
+  });
+});
